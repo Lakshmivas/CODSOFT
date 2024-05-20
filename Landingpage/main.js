d9a41b7769e1af1fbf734ef22501ad2a26f54b3b
Lakshmivas <168801420+Lakshmivas@users.noreply.github.com>
2024-05-20 20:28:09 +0530

    Add files via upload

---
+++ Landingpage/main.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+    // Here you can add more functionality like sending the form data to an email service
+});
